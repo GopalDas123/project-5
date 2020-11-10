4b4bb5837f07c24a6f818c2ebbdf26d0e258e71b
GopalDas123 <Gopaldax@gmail.com>
2020-11-11 02:26:33 +0500

    Responsive & Home Page Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import Navbarr from './Components/Navbar'
-import gif from './Components/Business Plan.gif'
-
+import React, { useEffect } from "react";
+import "./App.css";
+import Navbarr from "./Components/Navbar";
+import gif from "./Components/Business Plan.gif";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
     <div className="App">
-      <Navbarr/>
-      <div className="cont">
-        <p>Winsure assures you the quality</p>
-      <img src={gif} className="gif" alt=""/>
+      <Navbarr style={{ width: "100%" }} />
+      <div data-aos="fade-right" className="cont">
+        <p  className="tagLine">
+          Winsure Assures
+          <br />
+          You The Quality
+          <p className="tagLine2">
+            We Have The Great Developers Around The Globe
+          </p>
+          <button data-aos="zoom-in" className="start">Get Started</button>
+        </p>
+        <img src={gif}  data-aos="zoom-in" className="gif" alt="" />
       </div>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,35 @@
 import React from "react";
-// import './Nav.css'
-import {
-  Navbar,
-  Nav,
-  //   NavDropdown,
-  //   Form,
-  //   FormControl,
-  //   Button,
-} from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 const Navbarr = () => {
   return (
-    <div>
-      <Navbar variant="dark" bg="danger" expand="lg">
-        <Navbar.Brand href="#home">WINSURE</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse  id="basic-navbar-nav">
-          <Nav style={{marginLeft:'81%'}} className="mr-auto" >
-            <Nav.Link href="#home" style={{color:'white',textTransform:'uppercase'}}>Home</Nav.Link>
-            <Nav.Link href="#link" style={{color:'white',textTransform:'uppercase'}}>About</Nav.Link>
-            <Nav.Link href="#link" style={{color:'white',textTransform:'uppercase'}}>Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Navbar variant="dark" bg="danger" expand="lg">
+      <Navbar.Brand href="#home">
+        <b>WINSURE</b>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav style={{ marginLeft: "75%" }} className="mr-auto">
+          <Nav.Link
+            href="#home"
+            style={{ color: "white", textTransform: "uppercase" }}
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            href="#link"
+            style={{ color: "white", textTransform: "uppercase" }}
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            href="#link"
+            style={{ color: "white", textTransform: "uppercase" }}
+          >
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
